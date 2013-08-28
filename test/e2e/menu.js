fb5ea0187c2d7ca3dbe7af70436e347dbef75f7c
Jacob-Friesen <jacob@jacobfriesen.com>
2013-08-27 21:00:11 -0400

    Added menu colour checking

--- test/e2e/menu.js
+++ test/e2e/menu.js
@@ -1,5 +1,13 @@
 describe('Menu', function(){
-    it('should start with home item coloured', function(){
+    function checkPageColoured(checkPage){
+        ['home', 'skills', 'experience', 'demos', 'blog'].forEach(function(page){
+            var extra = (page !== checkPage) ? '_grey' : '',
+                url = 'url(http://localhost:9876/images/menu_icons/'+page+'_page'+extra+'.png)'
+            expect(element('#' + page + '_button > a > div').css('background-image')).toBe(url);
+        });
+    }
+
+    it('should start with home item coloured (when on default page)', function(){
         // Need to make sure the page loads
         browser().navigateTo('/');
         sleep(0.5);
@@ -7,10 +15,29 @@ describe('Menu', function(){
         // sanity check
         expect(browser().location().url()).toBe('/home');
 
+        checkPageColoured('home');
+    });
+
+    it('should make the corresponding image to the colored position on page load', function(){
         ['home', 'skills', 'experience', 'demos', 'blog'].forEach(function(page){
-            var extra = (page !== 'home') ? '_grey' : '',
-                url = 'url(http://localhost:9877/images/menu_icons/'+page+'_page'+extra+'.png)'
-            expect(element('#'+page + '_button > a > div').css('background-image')).toBe(url);
+            browser().navigateTo('/' + page);
+            sleep(0.5);
+
+            expect(browser().location().url()).toBe('/' + page);
+            checkPageColoured(page);
+        });
+    });
+
+    it('should make the corresponding image to the colored position on going to a page', function(){
+        browser().navigateTo('/');
+        sleep(0.5);
+
+        ['home', 'skills', 'experience', 'demos', 'blog'].forEach(function(page){
+            element('#' + page + '_button > a').click();
+            sleep(0.2);
+
+            expect(browser().location().url()).toBe('/' + page);
+            checkPageColoured(page);
         });
     });
 });
