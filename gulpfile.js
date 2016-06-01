f80833f6116790259e274860ad83c4b8080ebe69
Jacob Friesen <jacob@jacobfriesen.com>
2016-06-01 01:24:02 -0400

    (feat) #18 Added pug files and jacob.json for quicker template building

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,48 @@
 'use strict';
 
+var fs = require('fs');
+
 var gulp = require('gulp');
+var data = require('gulp-data');
+var pug = require('gulp-pug');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 
 // CSS Specific
 
-var TEMPLATES = 'src/scss/**/*.scss';
+var CSS_TEMPLATES = 'src/scss/**/*.scss';
 
 gulp.task('scss', function() {
-  return gulp.src([TEMPLATES])
+  return gulp.src([CSS_TEMPLATES])
              .pipe(sass().on('error', sass.logError))
              .pipe(gulp.dest('src/css'));
 });
 
 gulp.task('watch-css', function() {
-  gulp.watch(TEMPLATES, ['scss']);
+  gulp.watch(CSS_TEMPLATES, ['scss']);
+});
+
+// HTML Specific
+
+var HTML_TEMPLATES = 'src/app/**/*.pug';
+
+gulp.task('pug', function() {
+  return gulp.src('src/app/**/*.pug')
+    .pipe(data(function(file) {
+      return JSON.parse(fs.readFileSync('./src/jacob.json'));
+    }))
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(rename(function(path) {
+        path.extname = '.html';
+        return path;
+    }))
+    .pipe(gulp.dest('src/app'));
+});
+
+gulp.task('watch-pug', function() {
+  gulp.watch(HTML_TEMPLATES, ['pug']);
 });
 
 // JS Specific
