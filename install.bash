66c6e9fd1bd56073388cf77b1341c4bfc3f8ccc8
Jacob Friesen <jacob@jacobfriesen.com>
2016-06-09 02:22:29 -0400

    (feat) #18 Now customServer is copied and does not conflict with ng serve

--- install.bash
+++ install.bash
@@ -5,7 +5,10 @@ echo "This assumes the Node.js and other basics are installed"
 npm install -g angular-cli
 npm install
 
+echo
+echo "File copies."
 cp "node_modules/jacob-friesens-resume/jacob.json" "src/jacob.json"
+cp "customServer.js" "dist/server.js"
 
 echo
 echo "Installation Complete."
