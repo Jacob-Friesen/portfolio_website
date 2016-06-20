cf48198d2b82c99299f76a1593f809d040c4b14b
Jacob Friesen <jacob@jacobfriesen.com>
2016-06-19 20:56:14 -0400

    (feat) #18 The lightbox is now in its own component
    - Started removing unused onInit

--- src/app/+demos/demos.component.ts
+++ src/app/+demos/demos.component.ts
@@ -1,39 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CollapseManagerService } from '../collapse-manager.service';
+import { LightboxComponent } from '../lightbox';
 
 @Component({
   moduleId: module.id,
   selector: 'app-demos',
   templateUrl: 'demos.component.html',
   styleUrls: ['demos.component.css'],
+  directives: [LightboxComponent],
   providers: [CollapseManagerService]
 })
-export class DemosComponent implements OnInit {
+export class DemosComponent {
   constructor (public collapseManager: CollapseManagerService) {}
-
-  ngOnInit() {
-  }
-
-  onImageClick(image) {
-    const modalImage = 'modal-image';
-    const modalLoadingMessage = 'loading-message';
-    const largeImage = image.replace('.png', '.large.png').replace('.jpg', '.large.jpg');
-
-    var modal = new window['tingle'].modal({
-      footer: false,
-      stickyFooter: false,
-      cssClass: ['image-lightbox'],
-      onOpen: function() {
-        modal.resize();
-        document.getElementById(modalImage).className += ' loaded';
-        document.getElementById(modalLoadingMessage).style.display = 'none';
-      }
-    });
-
-    modal.setContent(`
-      <h1 id="${modalLoadingMessage}" class="loading-message">Loading...</h1>
-      <img id="${modalImage}" src="${largeImage}"></img>`
-    );
-    modal.open();
-  }
 }
--- src/app/lightbox/lightbox.component.ts
+++ src/app/lightbox/lightbox.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'app-lightbox',
+  selector: 'lightbox',
+  inputs: ['image'],
   templateUrl: 'lightbox.component.html',
   styleUrls: ['lightbox.component.css']
 })
-export class LightboxComponent implements OnInit {
+export class LightboxComponent {
+
+  modalLoadingMessageId: string;
+  modalImageId: string;
 
   constructor() {}
 
-  ngOnInit() {
-  }
+  /**
+   * Opens a Lighbox style modal with the large version of the sent in image. The modal is based on the Tingle modal
+   * which can be found here: http://robinparisi.github.io/tingle/
+   * 
+   * @param {string} image The image to base the full size image load on. The full size images will have .large.<type>
+   */
+  onImageClick(image) {
+    const largeImage = image.replace('.png', '.large.png').replace('.jpg', '.large.jpg');
 
+    this.modalImageId = 'modal-image';
+    this.modalLoadingMessageId = 'loading-message';
+
+    var modal = new window['tingle'].modal({
+      footer: false,
+      stickyFooter: false,
+      cssClass: ['image-lightbox'],
+      onOpen: () => {
+        modal.resize();
+        document.getElementById(this.modalImageId).className += ' loaded';
+        document.getElementById(this.modalLoadingMessageId).style.display = 'none';
+      }
+    });
+
+    modal.setContent(`
+      <h1 id="${this.modalLoadingMessageId}" class="loading-message">Loading...</h1>
+      <img id="${this.modalImageId}" src="${largeImage}"></img>
+    `);
+    modal.open();
+  }
 }
