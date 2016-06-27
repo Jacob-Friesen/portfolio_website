b36cf512a379f05a167cf5c68d17b4c832ea4825
Jacob Friesen <jacob@jacobfriesen.com>
2016-06-26 23:03:22 -0400

    (feat) #24 E2Es no longer use sleep
    - Resulted in a 30% speed increase

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -11,6 +11,7 @@ export class TopSection {
 export class Page {
   title = element(by.id('title'));
   menuId = null;
+  pageElement = null;
 
   /**
    * Goes to the page specified in menuId for the inherited page.
@@ -22,7 +23,7 @@ export class Page {
       throw('The inherited page must specify a menuId');
     }
     element(by.id(this.menuId)).click();
-    browser.sleep(100); // Add a better page load later.
+    browser.wait(protractor.ExpectedConditions.presenceOf(this.pageElement), 5000);
 
     return this;
   }
@@ -32,85 +33,110 @@ export class Page {
 
 export class HomePage extends Page {
   menuId = 'home-menu-item';
+  pageElement = element(by.css('.page.home'));
   profileImage = element(by.id('profile-image'));
 }
 
 export class CollapsiblePage extends Page {
   currentDescription = element(by.id('card-description'));
   headers = element.all(by.css('.card-header'));
-  modalImage = element(by.id('modal-image'));
-  modalClose = element(by.css('.tingle-modal__close'));
 
   /**
    * Open the card at the specified index.
    *
    * @param {number} index The card to open. Starts at 0.
+   * @param {boolean} closing If this is for closing a card or not.
    */
-  toggleCardAt(index) {
+  toggleCardAt(index: number, closing: boolean) {
     this.headers.get(index).click();
-    // Wait for it to expand
-    browser.sleep(300);
+    var method = closing === true ? 'invisibilityOf' : 'presenceOf';
+    browser.wait(protractor.ExpectedConditions[method](this.currentDescription), 5000);
+  }
+
+  /**
+   * Waits until the given popover has the given text.
+   *
+   * @param {string} text The text to check for.
+   */
+  checkCardText(text: string) {
+    browser.wait(protractor.ExpectedConditions.textToBePresentInElement(this.currentDescription, text), 5000);
   }
 }
 
 export class ExperiencePage extends CollapsiblePage {
-  currentDescription = element(by.id('experience-description'));
   menuId = 'experience-menu-item';
+  pageElement = element(by.css('.page.experiences'));
+  currentDescription = element(by.id('experience-description'));
 }
 
 export class SkillsPage extends CollapsiblePage {
+  menuId = 'skills-menu-item';
+  pageElement = element(by.css('.page.skills'));
   currentDescription = element(by.id('skill-summary'));
   headers = element.all(by.css('.skillset-header'));
   subHeaders = element.all(by.css('.skill-header'));
-  menuId = 'skills-menu-item';
+
+  /**
+   * Waits for any skill to open on the page.
+   */
+  waitForSkillOpen() {
+    browser.wait(protractor.ExpectedConditions.presenceOf(this.currentDescription), 5000);
+  }
 
   /**
    * Open the sub skill at the specified index.
    *
    * @param {number} index The card to open. Starts at 0.
+   * @param {boolean} closing If this is for closing a subskill or not.
    */
-  toggleSubSkillAt(index) {
+  toggleSubSkillAt(index: number, closing: boolean) {
     this.subHeaders.get(index).click();
-    // Wait for it to expand
-    browser.sleep(300);
+    var method = closing === true ? 'invisibilityOf' : 'presenceOf';
+    browser.wait(protractor.ExpectedConditions[method](this.currentDescription), 5000);
   }
 }
 
 export class DemosPage extends CollapsiblePage {
+  menuId = 'demos-menu-item';
+  pageElement = element(by.css('.page.demos'));
   summary = element(by.id('summary'));
   currentDescription = element(by.id('demo-description'));
-  menuId = 'demos-menu-item';
+  modalImage = element(by.id('modal-image'));
+  modalClose = element(by.css('.tingle-modal__close'));
 
   /**
    * Open the lightbox at the specified index.
    *
    * @param {number} index The lightbox to open. Starts at 0.
    */
-  openLightboxAt(index) {
+  openLightboxAt(index: number) {
     element(by.id('lightbox-' + index)).element(by.tagName('img')).click();
-    // Wait for the animations to complete
-    browser.sleep(500);
   }
 
   /**
    * Close the currently open lightbox.
-   *
-   * @param {number} index The lightbox to open. Starts at 0.
    */
   closeLightbox() {
     this.modalClose.click();
-    // Wait for the animations to complete
-    browser.sleep(300);
+  }
+
+  waitUntilImagePresent() {
+    browser.wait(protractor.ExpectedConditions.presenceOf(this.modalImage), 5000);
+  }
+
+  waitUntilImageNotPresent() {
+    browser.wait(protractor.ExpectedConditions.invisibilityOf(this.modalImage), 5000);
   }
 }
 
 export class BlogPage extends Page {
-  title = element(by.id('post-title'));
   menuId = 'blog-menu-item';
+  pageElement = element(by.css('.page.blog'));
+  title = element(by.id('post-title'));
 }
 
 export class ResumePage extends Page {
   menuId = 'resume-menu-item';
-
+  pageElement = element(by.css('.page.resume'));
   title = element(by.css('.resume-body .header h1'));
 }
