21501ad37e0f212a3640fd027646c3d1b2032dd1
Jacob Friesen <jacob@jacobfriesen.com>
2016-08-13 12:39:50 -0400

    (feat) #39 Initial Upgrade to Angular CLI Webpack
    - A lot of things missing still like building and gulp reintegration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { MenuComponent } from './menu';
 import { ROUTER_DIRECTIVES} from '@angular/router';
 
 @Component({
-  moduleId: module.id,
+  moduleId: module.id + '',
   selector: 'portfolio-website-app',
-  templateUrl: 'portfolio-website.component.html',
+  templateUrl: 'app.component.html',
   directives: [ROUTER_DIRECTIVES, MenuComponent]
 })
-export class PortfolioWebsiteAppComponent implements OnInit {
+export class AppComponent implements OnInit {
   // This creates a class that causes CSS to trigger only after load after at least one page has loaded. There seems to
   // be no angular specific way to check page loads.
   ngOnInit() {
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,3 @@
-export * from './environment';
-export * from './portfolio-website.component';
+export * from './environments/environment';
+export * from './app.component';
+export * from './app.module';
