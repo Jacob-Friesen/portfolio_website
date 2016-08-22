0eda9a5aa7a5ca7fd18f1c9514f9dd537aab6a39
Jacob Friesen <jacob@jacobfriesen.com>
2016-08-21 20:41:58 -0400

    (feat) #39 All tests including the blog remote service now pass
    - Just some new linting and warnings to fix

--- src/app/blog-remote.service.spec.ts
+++ src/app/blog-remote.service.spec.ts
@@ -1,37 +1,37 @@
-// import { addProviders, inject } from '@angular/core/testing';
-// import { provide } from '@angular/core';
-// import { JSONPBackend, Response, ResponseOptions } from '@angular/http';
-// import { MockBackend, MockConnection } from '@angular/http/testing';
-// import { BlogRemoteService } from './blog-remote.service';
-// import { JSONP_PROVIDERS } from '@angular/http';
-// import 'rxjs/Rx';
+import { addProviders, inject } from '@angular/core/testing';
+import { provide } from '@angular/core';
+import { JSONPBackend, Response, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BlogRemoteService } from './blog-remote.service';
+import { JSONP_PROVIDERS } from '@angular/http';
+import 'rxjs/Rx';
 
-// describe('BlogRemote Service', () => {
-//   beforeEach(() => {
-//     addProviders([
-//       JSONP_PROVIDERS,
-//       provide(JSONPBackend, {useClass: MockBackend}),
-//       BlogRemoteService
-//     ]);
-//   });
+describe('BlogRemote Service', () => {
+  beforeEach(() => {
+    addProviders([
+      JSONP_PROVIDERS,
+      provide(JSONPBackend, {useClass: MockBackend}),
+      BlogRemoteService
+    ]);
+  });
 
-//   describe('getPosts', function() {
-//     it('should return a promise that gets all the posts then takes the first one',
-//       inject([JSONPBackend, BlogRemoteService], (mock: MockBackend, service: BlogRemoteService) => {
+  describe('getPosts', function() {
+    it('should return a promise that gets all the posts then takes the first one',
+      inject([JSONPBackend, BlogRemoteService], (mock: MockBackend, service: BlogRemoteService) => {
 
-//       mock.connections.subscribe((connection: MockConnection) => {
-//         connection.mockRespond(new Response(
-//           new ResponseOptions({
-//             body: {
-//               posts: [{ first: 'post' }, { second: 'post' }]
-//             }
-//           })
-//         ));
-//       });
+      mock.connections.subscribe((connection: MockConnection) => {
+        connection.mockRespond(new Response(
+          new ResponseOptions({
+            body: {
+              posts: [{ first: 'post' }, { second: 'post' }]
+            }
+          })
+        ));
+      });
 
-//       service.getPost().subscribe((res) => {
-//         expect(res).toEqual({ first: 'post' });
-//       });
-//     }));
-//   });
-// });
+      service.getPost().subscribe((res) => {
+        expect(res).toEqual({ first: 'post' });
+      });
+    }));
+  });
+});
