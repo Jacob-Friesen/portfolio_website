7bac555685454a9649fcde5cecda735ec8c5b5c7
Jacob Friesen <jacob@jacobfriesen.com>
2017-03-14 23:37:30 -0500

    (feat) #43 First part of tests updated
    - Made core software versions fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,26 @@
-import { addProviders, inject } from '@angular/core/testing';
+// Custom elements when provided as a schema prevents sub component loads
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from '../app/app.component';
 
+let fixture;
+
 beforeEach(() => {
-  addProviders([AppComponent]);
+  // addProviders([AppComponent]);
+  TestBed.configureTestingModule({
+    declarations: [
+      AppComponent
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  }).compileComponents();
+});
+beforeEach(() => {
+  fixture = TestBed.createComponent(AppComponent);
 });
 
 describe('App (Base)', () => {
-  it('should create the app',
-      inject([AppComponent], (app: AppComponent) => {
+  it('should create the app', () => {
+    let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
+  });
 });
