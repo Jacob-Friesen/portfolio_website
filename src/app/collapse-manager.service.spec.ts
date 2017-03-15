ba44263ecff02da3c30d93e6dfaee19937e5989b
Jacob Friesen <jacob@jacobfriesen.com>
2017-03-14 23:50:17 -0500

    (feat) #43 Basic collapse manager service test

--- src/app/collapse-manager.service.spec.ts
+++ src/app/collapse-manager.service.spec.ts
@@ -1,31 +1,26 @@
-import { addProviders, inject } from '@angular/core/testing';
 import { CollapseManagerService } from './collapse-manager.service';
 
 describe('CollapseManager Service', () => {
-  beforeEach(() => {
-    addProviders([CollapseManagerService]);
+  let service: CollapseManagerService;
+  beforeEach(() => service = new CollapseManagerService());
+
+  it('should start with the first item as open', () => {
+    expect(service.openIndex).toEqual(0);
   });
 
-  it('should start with the first item as open', inject([CollapseManagerService], (service: CollapseManagerService) => {
-    expect(service.openIndex).toEqual(0);
-  }));
-
   describe('toggle', function() {
-    it('should set the open index to the current when it is not the sent in index',
-      inject([CollapseManagerService], (service: CollapseManagerService) => {
+    it('should set the open index to the current when it is not the sent in index', () => {
 
       service.toggle(1);
       expect(service.openIndex).toEqual(1);
 
       service.toggle(3);
       expect(service.openIndex).toEqual(3);
-    }));
+    });
 
-    it('should set the open index to -1 when it is the sent in index',
-      inject([CollapseManagerService], (service: CollapseManagerService) => {
-
+    it('should set the open index to -1 when it is the sent in index', () => {
       service.toggle(0);
       expect(service.openIndex).toEqual(-1);
-    }));
+    });
   });
 });
