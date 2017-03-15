147b1a303354201759acb34cc6c84916436d2a16
Jacob Friesen <jacob@jacobfriesen.com>
2017-03-15 00:36:18 -0500

    (feat) #43 Blog api testing (remote jsonp testing support)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,22 +3,26 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from '../app/app.component';
 
-let fixture;
-
-beforeEach(() => {
-  // addProviders([AppComponent]);
-  TestBed.configureTestingModule({
-    declarations: [
-      AppComponent
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
-  }).compileComponents();
-});
-beforeEach(() => {
-  fixture = TestBed.createComponent(AppComponent);
-});
-
 describe('App (Base)', () => {
+  let fixture;
+
+  beforeEach(() => {
+    // addProviders([AppComponent]);
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents(() => {
+      fixture = TestBed.createComponent(AppComponent);
+    });
+  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+  });
+
+  afterEach(() => TestBed.resetTestingModule());
+
   it('should create the app', () => {
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
