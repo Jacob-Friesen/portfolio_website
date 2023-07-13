d5594472b270025488671cf29f8511a2e13f7b60
Jacob-Friesen <jacob@jacobfriesen.com>
2023-07-12 21:34:54 -0400

    V6 #50 Initial branch git reset --soft HEAD~1#	deleted:    src/app/environment.ts

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/jacobfriesen/Documents/work/portfolio_website/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    Hello World!
+
+    <NuxtWelcome />
+  </div>
+</template>
