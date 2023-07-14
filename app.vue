08757d208c79a1e9addeb81d0a4ddfc4e5e4fdfd
Jacob-Friesen <jacob@jacobfriesen.com>
2023-07-14 01:02:38 -0400

    V6 #50 Production run with API test

--- app.vue
+++ app.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    Hello World!
-
     <NuxtWelcome />
+    <Hello />
   </div>
 </template>
---
+++ components/Hello.vue
@@ -0,0 +1,7 @@
+<script setup>
+const {data} = await useFetch('/hello');
+</script>
+
+<template>
+  Data: <pre>{{data}}</pre>
+</template>
