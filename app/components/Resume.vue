588730f51d93e884108fe1ff8c6aaa72a8ab715d
Jacob-Friesen <jacob@jacobfriesen.com>
2026-01-14 21:54:10 -0500

    V6 (#50) Add cache busting to resume iframe

--- app/components/Resume.vue
+++ app/components/Resume.vue
@@ -11,8 +11,25 @@
   }
 </style>
 
+<script>
+export default {
+  data() {
+    return {
+      resumeSrc: '/Jacob_Friesen_Resume_No_Title.html',
+      timestamp: Date.now(),
+    };
+  },
+  computed: {
+    iframeSrc() {
+      // Append the timestamp as a query parameter for cache busting
+      return `${this.resumeSrc}?v=${this.timestamp}`;
+    },
+  },
+};
+</script>
+
 <template>
   <ResumeDownload class="resume-download-btn"/>
   
-  <iframe class="resume" width="100%"src="/Jacob_Friesen_Resume_No_Title.html"></iframe>
+  <iframe class="resume" width="100%" :src="iframeSrc"></iframe>
 </template>
